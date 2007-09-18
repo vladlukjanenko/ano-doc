e36218339a4ba96095afa0f2f578da6b29871f48
Leon Rosenberg <leon.rosenberg@anotheria.net>
2007-09-18 14:06:53 +0000

    added DBContext

--- java/net/anotheria/anodoc/util/context/CallContext.java
+++ java/net/anotheria/anodoc/util/context/CallContext.java
@@ -1,11 +1,14 @@
 package net.anotheria.anodoc.util.context;
 
+
 public abstract class CallContext {
 	
 	private String currentLanguage;
+	private DBContext dbContext; 
 	
 	public void reset(){
 		currentLanguage = null;
+		dbContext = null;
 	}
 
 	public String getCurrentLanguage() {
@@ -17,8 +20,18 @@ public abstract class CallContext {
 	}
 	
 	public abstract String getDefaultLanguage();
-		
 	
-
+	public void setDbContext(DBContext aDbContent){
+		dbContext = aDbContent;
+	}
+	
+	public DBContext getDbContext(){
+		//this is not really thread-safe, but its better to risk to create one dummy object as to 
+		//synchronize here. Especially since the CallContext is ThreadLocal and therefore shouldn't 
+		//be accessed from many threads at once.
+		if (dbContext==null)
+			dbContext = new DBContext();
+		return dbContext;
+	}
 }
 
---
+++ java/net/anotheria/anodoc/util/context/DBContext.java
@@ -0,0 +1,36 @@
+package net.anotheria.anodoc.util.context;
+
+public class DBContext {
+	private String tableNamePrefix;
+	private String tableNamePostfix;
+	
+	public static final String DELIMITER = "_";
+	
+	public DBContext(){
+		tableNamePostfix = tableNamePrefix = "";
+	}
+	
+	public String getTableNamePostfix() {
+		return tableNamePostfix;
+	}
+	public void setTableNamePostfix(String tableNamePostfix) {
+		this.tableNamePostfix = tableNamePostfix;
+	}
+	public String getTableNamePrefix() {
+		return tableNamePrefix;
+	}
+	public void setTableNamePrefix(String tableNamePrefix) {
+		this.tableNamePrefix = tableNamePrefix;
+	}
+	
+	public String getTableNameInContext(String tableName){
+		StringBuilder ret = new StringBuilder();
+		if (tableNamePrefix!=null && tableNamePrefix.length()>0)
+			ret.append(tableNamePrefix).append(DELIMITER);
+		ret.append(tableName);
+		if (tableNamePostfix!=null && tableNamePostfix.length()>0)
+			ret.append(tableNamePostfix).append(DELIMITER);
+		
+		return ret.toString();
+	}
+}
