2f971ac3195a13b8a704f0fdd5c63077f6f27381
Leon Rosenberg <leon.rosenberg@anotheria.net>
2007-09-18 14:07:10 +0000

    comments

--- java/net/anotheria/asg/data/DataObject.java
+++ java/net/anotheria/asg/data/DataObject.java
@@ -1,12 +1,25 @@
 package net.anotheria.asg.data;
-
+/**
+ * Basic interface which all objects managed by the generated code will implement and which provides basic access methods.
+ * @author another
+ *
+ */
 public interface DataObject extends Cloneable{
 	public String getId();
 
 	public Object clone() throws CloneNotSupportedException;
-	
+
+	/**
+	 * Returns the value of a property in an underlying type.
+	 * @param propertyName
+	 * @return
+	 */
 	public Object getPropertyValue(String propertyName);
 	
+	/**
+	 * Returns the name under which the object was defined in xml originally.
+	 * @return
+	 */
 	public String getDefinedName();
 
 }
