20ad07106dd61bcfeb58ef123ace11e68ec3c6db
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-05-22 00:30:34 +0000

    checkstyle

--- java/net/anotheria/anodoc/service/IModuleFactory.java
+++ java/net/anotheria/anodoc/service/IModuleFactory.java
@@ -10,40 +10,38 @@ import net.anotheria.anodoc.data.Module;
  * able to create new modules.
  * It also should have the ability to create new Documents in the
  * Module Modell.
- * @since 1.0
- * @see biz.beaglesoft.bgldoc.service.AbstractModuleFactory
  */
 public interface IModuleFactory {
 	/**
-	 * Creates a new BGLModule Instance for given owner and copy ids.
+	 * Creates a new Module Instance for given owner and copy ids.
 	 */
-	public Module createModule(String ownerId, String copyId);
+	Module createModule(String ownerId, String copyId);
 	
 	/**
-	 * Creates a new BGLDocument with given name.
+	 * Creates a new Document with given name.
 	 */
-	public Document createDocument(String name);
+	Document createDocument(String name);
 	
 	/**
-	 * Creates a new BGLDocument with given name and a type identifier. 
+	 * Creates a new Document with given name and a type identifier. 
 	 */
-	public Document createDocument(String name, String typeIdentifier);
+	Document createDocument(String name, String typeIdentifier);
 	
 	/**
-	 * Creates a new BGLDocument with given name in a given context.
+	 * Creates a new Document with given name in a given context.
 	 * A context can be another document which holds the newly created document,
 	 * or a list.
 	 */
-	public Document createDocument(String name, DataHolder context);
+	Document createDocument(String name, DataHolder context);
 	
 	/**
-	 * Creates a new BGLDocumentList with given name and without a context. Normally
+	 * Creates a new DocumentList with given name and without a context. Normally
 	 * you don't need this method, since list is seldom overwritten. 
 	 */
-	public <D extends Document>DocumentList<D> createDocumentList(String name);
+	<D extends Document>DocumentList<D> createDocumentList(String name);
 	/**
-	 * Creates a new BGLDocumentList with given name and context.
+	 * Creates a new DocumentList with given name and context.
 	 */
-	public <D extends Document>DocumentList<D> createDocumentList(String name, DataHolder context);
+	<D extends Document>DocumentList<D> createDocumentList(String name, DataHolder context);
 	
 }
