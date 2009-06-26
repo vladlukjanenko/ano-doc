5dd7776af3fe3f123e72954669ba4684b97bde7f
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-06-26 10:10:11 +0000

    comments

--- java/net/anotheria/asg/generator/meta/MetaDocument.java
+++ java/net/anotheria/asg/generator/meta/MetaDocument.java
@@ -31,6 +31,10 @@ public class MetaDocument implements IGenerateable{
 		propertyNames = new HashSet<String>();
 	}
 	
+	/**
+	 * Adds a property to the document.
+	 * @param p property to add.
+	 */
 	public void addProperty(MetaProperty p){
 		if (propertyNames.contains(p.getName()))
 			throw new IllegalArgumentException("This document already contains a property or link with name "+p.getName());
@@ -39,6 +43,10 @@ public class MetaDocument implements IGenerateable{
 		properties.add(p);
 	}
 	
+	/**
+	 * Adds a link to the document.
+	 * @param l
+	 */
 	public void addLink(MetaLink l){
 		if (propertyNames.contains(l.getName()))
 			throw new IllegalArgumentException("This document already contains a property or link with name "+l.getName());
@@ -54,7 +62,7 @@ public class MetaDocument implements IGenerateable{
 	}
 
 	/**
-	 * @return
+	 * Returns all contained properties.
 	 */
 	public List<MetaProperty> getProperties() {
 		return properties;
@@ -102,11 +110,20 @@ public class MetaDocument implements IGenerateable{
 	public List<MetaProperty> getLinks(){
 		return links;
 	}
-	
+	/**
+	 * Return the name constant for the id holder for this document for cms storage.
+	 * @return
+	 */
 	public String getIdHolderName(){
 		return "ID_HOLDER_"+getName().toUpperCase();
 	}
 	
+	/**
+	 * Returns a field with given name. This can be a virtual field for the presentation (id, plainId, documentLastUpdateTimestamp, multilingualInstanceDisabled), a 
+	 * MetaProperty or a MetaLink. If nothing is found an IllegalArgumentException is thrown.
+	 * @param name name of the field.
+	 * @return
+	 */
 	public MetaProperty getField(String name){
 		
 		if (name.equals("id"))
@@ -135,6 +152,11 @@ public class MetaDocument implements IGenerateable{
 		throw new IllegalArgumentException("No such field: "+name+" in document "+getFullName());
 	}
 	
+	/**
+	 * Returns true if the document contains at list one comparable property or link.
+	 * TODO (leon) I have serious doubts whether this method does what its intended to do.
+	 * @return
+	 */
 	public boolean isComparable(){
 		for (int i=0; i<properties.size(); i++){
 			if (! (properties.get(i) instanceof MetaContainerProperty))
@@ -165,6 +187,11 @@ public class MetaDocument implements IGenerateable{
 		this.parentModule = parentModule;
 	}
 	
+	/**
+	 * Returns all internal links to the given document if any. This is useful if you need to find out at generation time whether one document is linked to another document.
+	 * @param anotherDocument the target document.
+	 * @return list of MetaLinks to the targetDocument or an empty list.
+	 */
 	public List<MetaLink> getLinksToDocument(MetaDocument anotherDocument){
 		List<MetaLink> ret = new ArrayList<MetaLink>();
 		for (MetaProperty p : links){
@@ -175,6 +202,10 @@ public class MetaDocument implements IGenerateable{
 		return ret;
 	}
 	
+	/**
+	 * Returns true if at least one of the properties or links are multilingual, false otherwise.
+	 * @return
+	 */
 	public boolean isMultilingual(){
 		for (MetaProperty p : properties){
 			if (p.isMultilingual())
