3b5d369a9b4bdddf7387d09ed175d36381b3d272
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-11-22 23:18:50 +0000

    test commit

--- java/net/anotheria/asg/util/filter/AZDocumentFilter.java
+++ java/net/anotheria/asg/util/filter/AZDocumentFilter.java
@@ -8,7 +8,7 @@ import net.anotheria.asg.data.DataObject;
 
 /**
  * Expose documents filtering by first letter of document name
- *
+ * (TEST COMMIT)
  */
 public class AZDocumentFilter implements DocumentFilter{
 	
